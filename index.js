80f2c7f7e2d6ed446afd4c395b47d286423cfcc3
cedcoss <you@example.com>
2022-03-31 13:49:31 +0530

    Final counter commit (added in js file)

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-let hi = "Hi";
-console.log(hi)
-hi="swat";
-console.log(hi)
+var count;
+
+        document.querySelector(".btn-plus").addEventListener("click",function(){
+            count=document.getElementById("main").value
+            count++;
+            document.getElementById("main").value=count;
+
+            
+}
+        
+        )
+        document.querySelector(".btn-minus").addEventListener("click",function(){
+            count=document.getElementById("main").value
+            count--;
+            document.getElementById("main").value=count;
+
+            
+}
+        )
